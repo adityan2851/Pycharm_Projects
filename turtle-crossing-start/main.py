07c2ae5168b01248766c20eff4fc7763d8820585
unknown <adhi2851@gmail.com>
2021-04-24 22:22:56 +0530

    turtle cross midway

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -7,8 +7,22 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+
+player = Player()
+screen.onkey(player.moveup, "Up")
+
+car_manager = CarManager()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_car()
+
+    # Collision
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+           game_is_on = False
--- turtle-crossing-start/player.py
+++ turtle-crossing-start/player.py
@@ -1,7 +1,19 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("turtle")
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+
+    def moveup(self):
+        self.forward(MOVE_DISTANCE)
+        if self.ycor() == FINISH_LINE_Y:
+            self.goto(STARTING_POSITION)
