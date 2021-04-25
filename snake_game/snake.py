86787928d2050f030074ee69eafa14ccdff27bc6
Adityan B <adhi2851@gmail.com>
2021-04-25 22:49:05 +0530

    snake game high_score add

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -9,6 +9,7 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+
 class Snake:
 
     def __init__(self):
@@ -27,10 +28,15 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
-
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
