54d269044ac104ea0c3074a60ba32a0f50452c22
Adityan <adi@Adityans-Mac-mini.local>
2021-06-07 20:48:15 +0530

    add exception handling - nato_alphabet

--- day26/main.py
+++ day26/main.py
@@ -2,10 +2,21 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-#TODO 1. Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 _dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter your name").upper()
-output = [_dict[letter] for letter in word]
-print(output)
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+def generate_phonetic():
+    word = input("Enter your name").upper()
+    try:
+        output = [_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(output)
+
+
+generate_phonetic()
